4835a214f3477e2aba575676bdecb250e196af6a
ARTVIRTUAL <thiagoncg1@gmail.com>
2015-05-22 22:16:36 -0300

    Adicionado Cubos coletaveis.

---
+++ projetoNave/Assets/Scripts/RotateCube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCube : MonoBehaviour {
+	public float velocidade_x;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		Transform.Rotate (new Vector3 (10, 40, 50) * Time.deltaTime);
+		transform.Rotate (new Vector3 (velocidade_x, 50, 30) * Time.deltaTime);
+	
+	}
+}
